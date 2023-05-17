1995e2af1b1b6a7b7305a27f91eb10a628626ff5
David Nash <dnashwilton@gmail.com>
2023-05-17 11:38:57 +0000

    Add if statements to js file.

---
+++ assets/js/script.js
@@ -0,0 +1,185 @@
+
+
+If Question = 1 {
+    '1/ The greatest compliment anyone could pay you would be to say:'
+    'A - What a great job you make of bringing up your family.'
+    'B - That you deserve to be at the top of your chosen career/profession.'
+    'C - How happy your lover looks since they met you.'
+}
+Else Question = 2 {
+    '2/ At Christmas, it is important for you too:'
+    'A - Have a traditional family celebration.'
+    'B - Send presents to essential business contacts.'
+    'C - Be alone with your partner.'
+}
+Else Question = 3 {
+    '3/ You have to give up your job to look after your children. You feel:'
+    'A - It is the right and proper way to proceed.'
+    'B - Frustrated that you will not be able to fulfil your ambitions.'
+    'C - Relieved when they go to school so that you have more time to spend with your partner.'
+}
+Else If Question = 4 {
+    '4/ Where do you want to live?'
+    'A - In the country, in fresh air and green countryside.'
+    'B - In a big city, where you can be at the heart of a thriving business community.'
+    'C - By the sea, with stunning sunsets to share with your soul mate.'
+}
+Else If Question = 5 {
+    '5/ You mostly dress in clothes that are:'
+    'A - Comfortable for knocking around the house.'
+    'B - Smart and create an impression of elegant efficiency.'
+    'C - Alluring and attractive to the opposite sex.'
+}
+Else If Question = 6 {
+    '6/ You prefer to eat:'
+    'A - A well-cooked meal at home.'
+    'B - In an expensive, fashionable restaurant.'
+    'C - In a candlelit, cosy bistro.'
+}
+Else If Question = 7 {
+    '7/ People whose homes are always in a mess tend to be:'
+    'A - Lazy and undisciplined.'
+    'B - Too busy with more important things.'
+    'C - Charmingly Bohemian.'
+}
+Else If Question = 8 {
+    '8/ Energy is well spent on:'
+    'A - Keeping your home spotless.'
+    'B - Talking at length to colleagues to keep your finger on the pulse at work.'
+    'C - Strenuous exercise and beauty programmes to keep in shape.'
+}
+Else If Question = 9 {
+    '9/ A person who is power and money-motivated is:'
+    'A - Missing out on the real rewards to be gained by family life.'
+    'B - A real '
+    go - getter ' deserving of admiration.'
+    'C - Losing touch with the joy of romance.'
+}
+Else If Question = 10 {
+    '10/ Someone who is prepared to devote themselves to finding an ideal mate is:'
+    'A - Chasing rainbows.'
+    'B - A romantic fool.'
+    'C - An idealist.'
+}
+Else If Question = 11 {
+    '11/ A secure and happy home and family life is:'
+    'A - All you could ever desire.'
+    'B - Not enough to satisfy you.'
+    'C - The way you like to think of your parents.'
+}
+Else If Question = 12 {
+    '12/ Most of your friends are:'
+    'A - Other parents.'
+    'B - People connected with work.'
+    'C - Other young couples.'
+}
+Else If Question = 13 {
+    '13/ You choose as a partner:'
+    'A - Someone you can trust to be a good parent.'
+    'B - A person who is independent and has their own career.'
+    'C - Someone you are madly in love with.'
+}
+Else If Question = 14 {
+    '14/ The pictures in your photograph album are of:'
+    'A - Several generations of family and friends.'
+    'B - Various achievements in your life - for example - winning prizes at school or university.'
+    'C - Yourself and your lover on holiday in various places.'
+}
+Else If Question = 15 {
+    '15/ The cosmetics and toiletries you use are primarily:'
+    'A - To keep your skin clean and fresh smelling.'
+    'B - To preserve a well-groomed and sophisticated image.'
+    'C - To make yourself alluring and attractive to the opposite sex.'
+}
+Else If Question = 16 {
+    '16/ The politician who would get your vote would be lobbying for:'
+    'A - Better education.'
+    'B - Cuts in income tax.'
+    'C - More support for the arts.'
+}
+Else If Question = 17 {
+    '17/ Most of your food budget goes on:'
+    'A - Meals for the family.'
+    'B - '
+    Expense account ' lunches.'
+    'C - Banquets for your lover.'
+}
+Else If Question = 18 {
+    '18/ When you are feeling emotional or upset, the cause more often than not is:'
+    'A - Problems at home.'
+    'B - Trouble at work.'
+    'C - Conflict with your partner.'
+}
+Else If Question = 19 {
+    '19/ Whom do you relate to most comfortably?'
+    'A - Relatives.'
+    'B - Business colleagues.'
+    'C - Your partner.'
+}
+Else If Question = 20 {
+    '20/ Your daydreams take the form of:'
+    'A - Decorating your '
+    dream ' home.'
+    'B - Being eminently successful in the career that you have chosen.'
+    'C - Going on a romantic voyage with your lover.'
+}
+Else If Question = 21 {
+    '21/ A woman whose lifestyle you admire is:'
+    'A - The Late Queen (Elizabeth II).'
+    'B - A former female Prime Minister (Margaret Thatcher).'
+    'C - The Late Princess of Wales (Lady Diana).'
+}
+Else If Question = 22 {
+    "22/ You feel 'in your element' where you are:"
+    "A - On home territory and surrounded by your loved ones."
+    "B - In an office which is equipped with the latest technology."
+    "C - Anywhere with your lover by your side to provide strength and support."
+}
+Else If Question = 23 Then {
+    "23/ Children should be brought up to believe in:"
+    "A - Making a stable relationship as a base for building a family."
+    "B - Making the most of whatever talents and ability they may have."
+    "C - Seeking out happiness and beauty."
+}
+Else If Question = 24 {
+    "24/ A gift you would really appreciate right now would be:"
+    "A - A new vacuum cleaner."
+    "B - Next year's diary."
+    "C - A recording of your favourite love song."
+}
+Else If Question = 25 {
+    "25/ What aspect of shopping do you find irritating?"
+    "A - Being patronized because you are a 'mere' housewife/husband."
+    "B - Time wasted standing in queues."
+    "C - Drabness in shops and lack of glamour generally."
+}
+Else If Question = 26 {
+    "26/ If you are forced, by financial circumstances to work, what do you do?"
+    "A - Choose something you can do at home, so as not interfere with your family life."
+    "B - You would never have to be forced, since you are already strongly motivated to work."
+    "C - Opt to do something that is artistic and creative."
+}
+Else If Question = 27 {
+    "27/ Birthdays can be pretty expensive when:"
+    "A - Youngsters in the family circle are growing up and want the latest fashions."
+    "B - There is a continuous stream of them at work."
+    "C - You want to spend all your money on a fabulous gift for your lover."
+}
+Else If Question = 28 {
+    "28/ One of the most important decisions you have made recently was:"
+    "A - Choosing a new house."
+    "B - Making up your mind on what career you want to follow."
+    "C - Deciding on getting married."
+}
+Else If Question = 29 {
+    "29/ A friend suggests you emigrate and join them in Australia. That would depend upon whether:"
+    "A - You could bear to leave some of your relatives behind."
+    "B - Your career would be advanced by going."
+    "C - Your partner would go with you."
+}
+Else If Question = 30 {
+    "30/ In a competition, you win the star prize of two weeks for two on a Caribbean Island. You:"
+    "A - Give the tickets as a honeymoon present to a couple in your family who are getting married."
+    "B - Ensure your promotion by offering them to your boss."
+    "C - Take off with your sweetheart."
+}
