7c1738162033f39ff1a0456d23f9047f45864724
David Nash <dnashwilton@gmail.com>
2023-05-20 11:35:26 +0000

    Add rest of quiz as const.

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,14 +2,14 @@ const questions = [{
         question: "1/ The greatest compliment anyone could pay you would be to say:",
         answera: "A - What a great job you make of bringing up your family.",
         answerb: "B - That you deserve to be at the top of your chosen career/profession.",
-        answerc = "C - How happy your lover looks since they met you.",
+        answerc: "C - How happy your lover looks since they met you.",
     },
 
     {
         question: "2/ At Christmas, it is important for you too:",
-        answera = "A - Have a traditional family celebration.",
-        answerb = "B - Send presents to essential business contacts."
-        answerc = "C - Be alone with your partner.",
+        answera: "A - Have a traditional family celebration.",
+        answerb: "B - Send presents to essential business contacts.",
+        answerc: "C - Be alone with your partner.",
     },
 
     {
@@ -35,9 +35,9 @@ const questions = [{
 
     {
         question: "6/ You prefer to eat:",
-        answera = "A - A well-cooked meal at home.",
-        answerb = "B - In an expensive, fashionable restaurant.",
-        answerc = "C - In a candlelit, cosy bistro.",
+        answera: "A - A well-cooked meal at home.",
+        answerb: "B - In an expensive, fashionable restaurant.",
+        answerc: "C - In a candlelit, cosy bistro.",
     },
 
     {
@@ -48,120 +48,162 @@ const questions = [{
     },
 
     {
-question: "8/ Energy is well spent on:",
-answera: "A - Keeping your home spotless.",
-answera: "B - Talking at length to colleagues to keep your finger on the pulse at work.",
-answerc: "C - Strenuous exercise and beauty programmes to keep in shape.",
+        question: "8/ Energy is well spent on:",
+        answera: "A - Keeping your home spotless.",
+        answera: "B - Talking at length to colleagues to keep your finger on the pulse at work.",
+        answerc: "C - Strenuous exercise and beauty programmes to keep in shape.",
+    },
+    {
+        question: "9/ A person who is power and money-motivated is:",
+        answera: "A - Missing out on the real rewards to be gained by family life.",
+        answerb: "B - A real 'go-getter' deserving of admiration.",
+        answerc: "C - Losing touch with the joy of romance.",
     },
 
-]
+    {
+        question: "10/ Someone who is prepared to devote themselves to finding an ideal mate is:",
+        answera: "A - Chasing rainbows.",
+        answerb: "B - A romantic fool.",
+        answerc: "C - An idealist.",
+    },
 
-lblQuestions = "9/ A person who is power and money-motivated is:"
-lblAnswerA = "A - Missing out on the real rewards to be gained by family life."
-lblAnswerB = "B - A real 'go-getter' deserving of admiration."
-lblAnswerC = "C - Losing touch with the joy of romance."
-ElseIf Question = 10 Then
-lblQuestions = "10/ Someone who is prepared to devote themselves to finding an ideal mate is:"
-lblAnswerA = "A - Chasing rainbows."
-lblAnswerB = "B - A romantic fool."
-lblAnswerC = "C - An idealist."
-ElseIf Question = 11 Then
-lblQuestions = "11/ A secure and happy home and family life is:"
-lblAnswerA = "A - All you could ever desire."
-lblAnswerB = "B - Not enough to satisfy you."
-lblAnswerC = "C - The way you like to think of your parents."
-ElseIf Question = 12 Then
-lblQuestions = "12/ Most of your friends are:"
-lblAnswerA = "A - Other parents."
-lblAnswerB = "B - People connected with work."
-lblAnswerC = "C - Other young couples."
-ElseIf Question = 13 Then
-lblQuestions = "13/ You choose as a partner:"
-lblAnswerA = "A - Someone you can trust to be a good parent."
-lblAnswerB = "B - A person who is independent and has their own career."
-lblAnswerC = "C - Someone you are madly in love with."
-ElseIf Question = 14 Then
-lblQuestions = "14/ The pictures in your photograph album are of:"
-lblAnswerA = "A - Several generations of family and friends."
-lblAnswerB = "B - Various achievements in your life - for example - winning prizes at school or university."
-lblAnswerC = "C - Yourself and your lover on holiday in various places."
-ElseIf Question = 15 Then
-lblQuestions = "15/ The cosmetics and toiletries you use are primarily:"
-lblAnswerA = "A - To keep your skin clean and fresh smelling."
-lblAnswerB = "B - To preserve a well-groomed and sophisticated image."
-lblAnswerC = "C - To make yourself alluring and attractive to the opposite sex."
-ElseIf Question = 16 Then
-lblQuestions = "16/ The politician who would get your vote would be lobbying for:"
-lblAnswerA = "A - Better education."
-lblAnswerB = "B - Cuts in income tax."
-lblAnswerC = "C - More support for the arts."
-ElseIf Question = 17 Then
-lblQuestions = "17/ Most of your food budget goes on:"
-lblAnswerA = "A - Meals for the family."
-lblAnswerB = "B - 'Expense account' lunches."
-lblAnswerC = "C - Banquets for your lover."
-ElseIf Question = 18 Then
-lblQuestions = "18/ When you are feeling emotional or upset, the cause more often than not is:"
-lblAnswerA = "A - Problems at home."
-lblAnswerB = "B - Trouble at work."
-lblAnswerC = "C - Conflict with your partner."
-ElseIf Question = 19 Then
-lblQuestions = "19/ Whom do you relate to most comfortably?"
-lblAnswerA = "A - Relatives."
-lblAnswerB = "B - Business colleagues."
-lblAnswerC = "C - Your partner."
-ElseIf Question = 20 Then
-lblQuestions = "20/ Your daydreams take the form of:"
-lblAnswerA = "A - Decorating your 'dream' home."
-lblAnswerB = "B - Being eminently successful in the career that you have chosen."
-lblAnswerC = "C - Going on a romantic voyage with your lover."
-ElseIf Question = 21 Then
-lblQuestions = "21/ A woman whose lifestyle you admire is:"
-lblAnswerA = "A - The Queen (Elizabeth II)."
-lblAnswerB = "B - The former Prime Minister (Margaret Thatcher)."
-lblAnswerC = "C - The Princess of Wales (Lady Diana)."
-ElseIf Question = 22 Then
-lblQuestions = "22/ You feel 'in your element' where you are:"
-lblAnswerA = "A - On home territory and surrounded by your loved ones."
-lblAnswerB = "B - In an office which is equipped with the latest technology."
-lblAnswerC = "C - Anywhere with your lover by your side to provide strength and support."
-ElseIf Question = 23 Then
-lblQuestions = "23/ Children should be brought up to believe in:"
-lblAnswerA = "A - Making a stable relationship as a base for building a family."
-lblAnswerB = "B - Making the most of whatever talents and ability they may have."
-lblAnswerC = "C - Seeking out happiness and beauty."
-ElseIf Question = 24 Then
-lblQuestions = "24/ A gift you would really appreciate right now would be:"
-lblAnswerA = "A - A new vacuum cleaner."
-lblAnswerB = "B - Next year's diary."
-lblAnswerC = "C - A recording of your favourite love song."
-ElseIf Question = 25 Then
-lblQuestions = "25/ What aspect of shopping do you find irritating?"
-lblAnswerA = "A - Being patronized because you are a 'mere' housewife/husband."
-lblAnswerB = "B - Time wasted standing in queues."
-lblAnswerC = "C - Drabness in shops and lack of glamour generally."
-ElseIf Question = 26 Then
-lblQuestions = "26/ If you are forced, by financial circumstances to work, what do you do?"
-lblAnswerA = "A - Choose something you can do at home, so as not interfere with your family life."
-lblAnswerB = "B - You would never have to be forced, since you are already strongly motivated to work."
-lblAnswerC = "C - Opt to do something that is artistic and creative."
-ElseIf Question = 27 Then
-lblQuestions = "27/ Birthdays can be pretty expensive when:"
-lblAnswerA = "A - Youngsters in the family circle are growing up and want the latest fashions."
-lblAnswerB = "B - There is a continuous stream of them at work."
-lblAnswerC = "C - You want to spend all your money on a fabulous gift for your lover."
-ElseIf Question = 28 Then
-lblQuestions = "28/ One of the most important decisions you have made recently was:"
-lblAnswerA = "A - Choosing a new house."
-lblAnswerB = "B - Making up your mind on what career you want to follow."
-lblAnswerC = "C - Deciding on getting married."
-ElseIf Question = 29 Then
-lblQuestions = "29/ A friend suggests you emigrate and join them in Australia. That would depend upon whether:"
-lblAnswerA = "A - You could bear to leave some of your relatives behind."
-lblAnswerB = "B - Your career would be advanced by going."
-lblAnswerC = "C - Your partner would go with you."
-ElseIf Question = 30 Then
-lblQuestions = "30/ In a competition, you win the star prize of two weeks for two on a Caribbean Island. You:"
-lblAnswerA = "A - Give the tickets as a honeymoon present to a couple in your family who are getting married."
-lblAnswerB = "B - Ensure your promotion by offering them to your boss."
-lblAnswerC = "C - Take off with your sweetheart."
+    {
+        question: "11/ A secure and happy home and family life is:",
+        answera: "A - All you could ever desire.",
+        answerb: "B - Not enough to satisfy you.",
+        answerc: "C - The way you like to think of your parents.",
+    },
+
+    {
+        question: "12/ Most of your friends are:",
+        answera: "A - Other parents.",
+        answerb: "B - People connected with work.",
+        answerc: "C - Other young couples.",
+    },
+
+    {
+        question: "13/ You choose as a partner:",
+        answera: "A - Someone you can trust to be a good parent.",
+        answerb: "B - A person who is independent and has their own career.",
+        answerb: "C - Someone you are madly in love with.",
+    },
+
+    {
+        question: "14/ The pictures in your photograph album are of:",
+        answera: "A - Several generations of family and friends.",
+        answerb: "B - Various achievements in your life - for example - winning prizes at school or university.",
+        answerb: "C - Yourself and your lover on holiday in various places.",
+    },
+
+    {
+        question: "15/ The cosmetics and toiletries you use are primarily:",
+        answera: "A - To keep your skin clean and fresh smelling.",
+        answerb: "B - To preserve a well-groomed and sophisticated image.",
+        answerc: "C - To make yourself alluring and attractive to the opposite sex.",
+    },
+
+    {
+        questions: "16/ The politician who would get your vote would be lobbying for:",
+        aAnswera: "A - Better education.",
+        answerb: "B - Cuts in income tax.",
+        answerC: "C - More support for the arts.",
+    },
+
+    {
+        question: "17/ Most of your food budget goes on:",
+        answera: "A - Meals for the family.",
+        answerb: "B - 'Expense account' lunches.",
+        answerc: "C - Banquets for your lover.",
+    },
+
+    {
+        question: "18/ When you are feeling emotional or upset, the cause more often than not is:",
+        answera: "A - Problems at home.",
+        answerb: "B - Trouble at work.",
+        answerc: "C - Conflict with your partner.",
+    },
+
+    {
+        question: "19/ Whom do you relate to most comfortably?",
+        answera: "A - Relatives.",
+        answerb: "B - Business colleagues.",
+        answerc: "C - Your partner.",
+    },
+
+    {
+        question: "20/ Your daydreams take the form of:",
+        answera: "A - Decorating your 'dream' home.",
+        answerb: "B - Being eminently successful in the career that you have chosen.",
+        answerc: "C - Going on a romantic voyage with your lover.",
+    },
+
+    {
+        question: "21/ A woman whose lifestyle you admire is:",
+        answera: "A - The Late Queen (Elizabeth II).",
+        answerb: "B - The former Prime Minister (Margaret Thatcher).",
+        answerc: "C - The Late Princess of Wales (Lady Diana).",
+    },
+
+    {
+        question: "22/ You feel 'in your element' where you are:",
+        answera: "A - On home territory and surrounded by your loved ones.",
+        answerb: "B - In an office which is equipped with the latest technology.",
+        answerc: "C - Anywhere with your lover by your side to provide strength and support.",
+    },
+
+    {
+        auestion: "23/ Children should be brought up to believe in:",
+        answera: "A - Making a stable relationship as a base for building a family.",
+        answerb: "B - Making the most of whatever talents and ability they may have.",
+        answerc: "C - Seeking out happiness and beauty.",
+    },
+
+    {
+        question: "24/ A gift you would really appreciate right now would be:",
+        answera: "A - A new vacuum cleaner.",
+        answerb: "B - Next year's diary.",
+        answerc: "C - A recording of your favourite love song.",
+    },
+
+    {
+        question: "25/ What aspect of shopping do you find irritating?",
+        answera: "A - Being patronized because you are a 'mere' housewife/husband.",
+        answerb: "B - Time wasted standing in queues.",
+        answerc: "C - Drabness in shops and lack of glamour generally.",
+    },
+
+    {
+question: "26/ If you are forced, by financial circumstances to work, what do you do?",
+answera: "A - Choose something you can do at home, so as not interfere with your family life.",
+answerb: "B - You would never have to be forced, since you are already strongly motivated to work.",
+answerc: "C - Opt to do something that is artistic and creative.",
+    },
+
+    {
+question: "27/ Birthdays can be pretty expensive when:",
+answera: "A - Youngsters in the family circle are growing up and want the latest fashions.",
+answerb: "B - There is a continuous stream of them at work.",
+answerc: "C - You want to spend all your money on a fabulous gift for your lover.",
+    },
+
+    {
+question: "28/ One of the most important decisions you have made recently was:",
+answera: "A - Choosing a new house.",
+answerb: "B - Making up your mind on what career you want to follow.",
+answerc: "C - Deciding on getting married.",
+    },
+
+    {
+questions: "29/ A friend suggests you emigrate and join them in Australia. That would depend upon whether:",
+answera: "A - You could bear to leave some of your relatives behind.",
+answerb: "B - Your career would be advanced by going.",
+answerc: "C - Your partner would go with you.",
+    },
+
+    {
+question: "30/ In a competition, you win the star prize of two weeks for two on a Caribbean Island. You:",
+answera: "A - Give the tickets as a honeymoon present to a couple in your family who are getting married.",
+answerb: "B - Ensure your promotion by offering them to your boss.",
+answerc: "C - Take off with your sweetheart.",
+    },
+]
