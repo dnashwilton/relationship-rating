4f5c979b30d7f233adcd0a64ca79a24435d4bf64
David Nash <dnashwilton@gmail.com>
2023-05-22 11:36:17 +0000

    Add functions and messages to js file.

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,7 @@ function relationships()
 
 {
     const questions = [
-        
+
         {
             question: "1/ The greatest compliment anyone could pay you would be to say:",
             answera: "A - What a great job you make of bringing up your family.",
@@ -221,30 +221,162 @@ function relationships()
 
 // Show next question in list.
 
-function displayNext() 
+function displayNext()
 
 {
     questionCount += 1;
 
+    function relationships()
+
     document.getElementById(questions) = questions.innerHTML;
-}
 
-// function to add 1 to answera, answerb or answerc
-function answerToQuestions() 
+    if questionCount === 31 {
 
-{
-    if (answera === true) {
-        answera = +1;
-    } else if (answerb === true) {
-        answerb = +1;
-    } else
-        (answerc === true); {
-        answerc += 1;
+        // function to add 1 to answera, answerb or answerc
+
+        answerToQuestions()
     }
-}
 
-// event listeners.
+    {
+        if (answera === true) {
+            answera = +1;
+        } else if (answerb === true) {
+            answerb = +1;
+        } else
+            (answerc === true); {
+            answerc += 1;
+        }
+    }
 
-answera.addEventListener("click", answerToQuestions);
-answerb.addEventListener("click", answerToQuestions);
-answerc.addEventListener("click", answerToQuestions);
+    // event listeners.
+
+    answera.addEventListener("click", answerToQuestions);
+    answerb.addEventListener("click", answerToQuestions);
+    answerc.addEventListener("click", answerToQuestions);
+
+    // Final score and message explaining the score.
+
+    function score() {
+
+        Message = "ANSWER A SCORE:- " + Str(answera) < br >
+            Message = Message + "ANSWER B SCORE:- " + Str(answerb) < br >
+            Message = Message + "ANSWER C SCORE:- " + Str(answerc) < br >
+            Message = Message + "This quiz was entitled 'What Makes You Tick - Career, Family or Romance." < br >
+            Message = Message + "The computer has added up the number of 'a', 'b' and 'c' answers you gave. Please read the following explanation."
+
+        if (AnswerA > AnswerB And AnswerA > AnswerC) {
+
+            Message = "MOSTLY A's" < br >
+                Message = Message + "Your major concern is with the family. You probably have children of your own or are planning to "
+            Message = Message + "start a family fairly soon. If not, then you are the kind of person who values the parental home, "
+            Message = Message + "gravitates there often and keeps in touch with brothers, sisters and other relatives. You are the "
+            Message = Message + "kind of person who would not want to be away from home at Christmas." < br >
+                Message = Message + "  If you are a woman you could be described as 'traditional', if a man, you would be seen as a "
+            Message = Message + "devoted husband, father or son who prefers to spend as much time as possible with his family." < br >
+                Message = Message + "CONTINUED ON NEXT PAGE"
+
+            Message = "CONTINUED FROM LAST PAGE" < br >
+                Message = Message + "  The main value of family life is probably stability and continuity and the identity that is derived "
+            Message = Message + "from it - the series of the long-term purpose. The chances are that you keep a photograph album full of "
+            Message = Message + "pictures of family events, such as holidays, birthdays, Christmas and Weddings which help you keep track "
+            Message = Message + "of 'where you come from' and also help you to 'know who you are'. You understand your roles and duties and "
+            Message = Message + "your self-esteem is greatly enhanced by the knowledge that other people need you." + LfCr + LfCr
+            Message = Message + "CONTINUED ON NEXT PAGE"
+
+            // MsgBox Message, DialogType, Title
+
+            Message = "CONTINUED FROM LAST PAGE" < br >
+                Message = Message + "REWARDS OF THE FAMILY" < br >
+                Message = Message + "It is easy to lose sight of these virtues when we read about all the violence that takes place "
+            Message = Message + "within families, observe the high divorce rate today, and hear people moan about lack of freedom "
+            Message = Message + "and the high cost of bringing up children. All these negative aspects are real, but families also "
+            Message = Message + "many rewards and satisfactions that are often taken for granted. If the family was all bad news (as "
+            Message = Message + "some radical psychiatrists seem to imply) there would be no need to feel sorry for orphans. But the "
+            Message = Message + "truth is we do: the family is the main source of 'base-camp' security and self-definition." < br >
+
+                // MsgBox Message, DialogType, Title
+
+        } else if {
+
+            AnswerB > AnswerA And AnswerB > AnswerC
+            Message = "MOSTLY B's" < br >
+                Message = Message + "The core of your value system focuses around your career ambitions. You probably had early success "
+            Message = Message + "and encouragement at school and have talents and abilities that you feel should not go to waste. You "
+            Message = Message + "may have embarked upon a career and climbed several rungs of the ladder already. If so, this is likely "
+            Message = Message + "to be within an occupation that is intrinsically interesting and creative and in which you can see prospects "
+            Message = Message + "for future progress and achievement. Actors, artists, scientists and business people, for example, are more "
+            Message = Message + "likely to devote themselves wholeheartedly to a career than typists or assembly workers. Jobs in the latter "
+            Message = Message + "categories are more often viewed as stop-gaps or unpleasant necessities." < br >
+                Message = Message + "CONTINUED ON NEXT PAGE"
+
+            // MsgBox Message, DialogType, Title
+
+            Message = "CONTINUED FROM LAST PAGE" < br >
+                Message = Message + "REWARDS OF A CAREER" < br >
+                Message = Message + "People who make successful careers are not only talented and achievement-oriented, they are usually extrovert "
+            Message = Message + "and socially skilled as well. It is very difficult to make progress in the business world, or any other field, "
+            Message = Message + "without meeting people and showing some degree  of diplomacy. Whether one is dealing with bosses, assistants, "
+            Message = Message + "rivals, clients or the public at large, some capacity to 'win friends and influence people' is usually a prerequisite. "
+            Message = Message + "Generally warmth and tact are innate traits, although some of the fundamental rules of how to deal with people can be "
+            Message = Message + "taught and applied consciously." < br >
+                Message = Message + "CONTINUED ON NEXT PAGE"
+
+            // MsgBox Message, DialogType, Title
+
+            Message = "CONTINUED FROM LAST PAGE" < br >
+                Message = Message + "  The rewards of career achievement are fairly obvious. Power, wealth and fame are satisfying in their own right, and carry "
+            Message = Message + "many spin-offs such as the opportunity to travel, dress well, eat in good restaurants, meet interesting people, and be "
+            Message = Message + "attractive to the opposite sex. Henry Kissinger is quoted as saying, 'Power is the greatest aphrodisiac'." < br >
+                Message = Message + "  Critics if this life-style say that the 'rat-race' leads to the neglect of friends and more important values, and if family "
+            Message = Message + "and romance are sacrificed too much, a certain emptiness is ultimately experienced."
+
+            // MsgBox Message, DialogType, Title
+
+        } else {
+
+            AnswerC > AnswerB And AnswerC > AnswerA Then
+            Message = "MOSTLY C's" < br >
+                Message = Message + "You are the type of person who lives primarily for love and romance. Perhaps you are 'in love' at "
+            Message = Message + "this moment and seeing the world through the proverbial rose-coloured spectacles. Some say this is "
+            Message = Message + "an illusion but others make an equal case that it the greatest 'reality'. Who can say which is more true?" < br >
+                Message = Message + "  Perhaps you have already emerged from an intense love affair that gave life a new meaning. Or you have "
+            Message = Message + "madly in love many times before, and know that life becomes much more exciting and exhilarating when you are." < br >
+                Message = Message + "CONTINUED ON NEXT PAGE"
+
+            // MsgBox Message, DialogType, Title
+
+            Message = "CONTINUED FROM LAST PAGE" < br >
+                Message = Message + "ROMANTIC AT HEART" < br >
+                Message = Message + "Even if you are not crazy for a particular partner, and never have been in the past, you are in love with the idea "
+            Message = Message + "of love and create it out of your imagination. You are an avid reader of romantic fiction who harks back to day of "
+            Message = Message + "medieval chivalry or Victorian dignity and elegance. You prefer to focus upon the beautiful and stimulating things "
+            Message = Message + "in life and ignore the pain, baring practicalities and dross." < br >
+                Message = Message + "CONTINUED ON NEXT PAGE"
+
+            // MsgBox Message, DialogType, Title
+
+            Message = "CONTINUED FROM LAST PAGE" < br >
+                Message = Message + "  Whatever your situation, you may count yourself fortunate, for it takes benevolence and a feeling of well-being "
+            Message = Message + "to love. The chances are you are still young, for a romantic view of the world is notoriously difficult to sustain "
+            Message = Message + "with age. On the other hand, as Barbara Cartland will attest, the belief that 'love is what makes the world go round' "
+            Message = Message + "is not necessarily the temporary preserve of youth."
+
+            // MsgBox Message, DialogType, Title
+
+            function (lastMessage)()
+
+        }
+
+    }
+
+    function lastMessage() {
+        If AnswerA > AnswerB And AnswerA > AnswerC Or AnswerB > AnswerA And AnswerB > AnswerC Or AnswerC > AnswerB And AnswerC > AnswerA Then
+        Message = "WHICH IS BEST" < br >
+            Message = Message + "It is not our intention to judge which of these three life philosophies is the most satisfying or virtuous. People "
+        Message = Message + "are different and must follow the dictates of their heart in such matters. Each orientation has its particular "
+        Message = Message + "rewards and drawbacks." < br >
+            Message = Message + "  Hopefully, what this quiz has achieved is to bring you to more explicit understanding of what motivates you in "
+        Message = Message + "in life and where your true values really lie." < br >
+
+            // MsgBox Message, DialogType, Title
+    }
