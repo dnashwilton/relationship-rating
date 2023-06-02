8b45704bdd8a49ff24ac9d73bcd7225c987fed73
David Nash <dnashwilton@gmail.com>
2023-06-02 14:06:43 +0000

    Placed Select Case in place for end of quiz.

--- assets/js/script.js
+++ assets/js/script.js
@@ -38,6 +38,69 @@ let questions = [
         choiceB : "Wrong",
         choiceC : "Correct",
         correct : "C"
+    },{
+        question : "What does HTML stand for?",
+        // imgSrc : "img/html.png",
+        choiceA : "Correct",
+        choiceB : "Wrong",
+        choiceC : "Wrong",
+        correct : "A"
+    },{
+        question : "What does CSS stand for?",
+        // imgSrc : "img/css.png",
+        choiceA : "Wrong",
+        choiceB : "Correct",
+        choiceC : "Wrong",
+        correct : "B"
+    },{
+        question : "What does JS stand for?",
+        // imgSrc : "img/js.png",
+        choiceA : "Wrong",
+        choiceB : "Wrong",
+        choiceC : "Correct",
+        correct : "C"
+    },{
+        question : "What does HTML stand for?",
+        // imgSrc : "img/html.png",
+        choiceA : "Correct",
+        choiceB : "Wrong",
+        choiceC : "Wrong",
+        correct : "A"
+    },{
+        question : "What does CSS stand for?",
+        // imgSrc : "img/css.png",
+        choiceA : "Wrong",
+        choiceB : "Correct",
+        choiceC : "Wrong",
+        correct : "B"
+    },{
+        question : "What does JS stand for?",
+        // imgSrc : "img/js.png",
+        choiceA : "Wrong",
+        choiceB : "Wrong",
+        choiceC : "Correct",
+        correct : "C"
+    },{
+        question : "What does HTML stand for?",
+        // imgSrc : "img/html.png",
+        choiceA : "Correct",
+        choiceB : "Wrong",
+        choiceC : "Wrong",
+        correct : "A"
+    },{
+        question : "What does CSS stand for?",
+        // imgSrc : "img/css.png",
+        choiceA : "Wrong",
+        choiceB : "Correct",
+        choiceC : "Wrong",
+        correct : "B"
+    },{
+        question : "What does JS stand for?",
+        // imgSrc : "img/js.png",
+        choiceA : "Wrong",
+        choiceB : "Wrong",
+        choiceC : "Correct",
+        correct : "C"
     }
 ];
 
@@ -46,10 +109,10 @@ let questions = [
 const lastQuestion = questions.length - 1;
 let runningQuestion = 0;
 let count = 0;
-const questionTime = 15; // 15s
-const gaugeWidth = 150; // 150px
-const gaugeUnit = gaugeWidth / questionTime;
-let TIMER;
+// const questionTime = 15; // 15s
+// const gaugeWidth = 150; // 150px
+// const gaugeUnit = gaugeWidth / questionTime;
+// let TIMER;
 let score = 0;
 
 // render a question
@@ -72,7 +135,7 @@ function startQuiz(){
     quiz.style.display = "block";
     renderProgress();
     renderCounter();
-    TIMER = setInterval(renderCounter,1000); // 1000ms = 1s
+    // TIMER = setInterval(renderCounter,1000); // 1000ms = 1s
 }
 
 // render progress
@@ -144,13 +207,45 @@ function scoreRender(){
     
     // calculate the amount of question percent answered by the user
     const scorePerCent = Math.round(100 * score/questions.length);
+
+    // Match case to answers
+
+    Select Case CorrectAnswers
+
+Case 0 To 3
+Message = "This score shows that your knowledge about sex is very basic. "
+Message = Message + "Finding out more about it can only be to your advantage."
+
+MsgBox Message, DialogType, Title
+
+Case 4 To 6
+Message = "Your knowledge of sex is not as good as it could be. Finding out as much "
+Message = Message + "as you can about sex can only help to improve your relationship with "
+Message = Message + "your partner."
+
+MsgBox Message, DialogType, Title
+
+Case 8 To 10
+Message = "THis is a fair score but you might like to learn more. Try to broaden your knowledge "
+Message = Message + "by reading more about the subject."
+
+MsgBox Message, DialogType, Title
+
+Case 11 To 12
+Message = "A very impressive performance. If you are as warm and considerate a lover as you are knnowledgeable "
+Message = Message = "about sex, your partner should be a very happy person."
+
+MsgBox Message, DialogType, Title
+
+End Select
+
     
     // choose the image based on the scorePerCent
-    let img = (scorePerCent >= 80) ? "img/5.png" :
-              (scorePerCent >= 60) ? "img/4.png" :
-              (scorePerCent >= 40) ? "img/3.png" :
-              (scorePerCent >= 20) ? "img/2.png" :
-              "img/1.png";
+    let img = (scorePerCent >= 80) ? "assets/images/05.png" :
+              (scorePerCent >= 60) ? "assets/images/04.png" :
+              (scorePerCent >= 40) ? "assets/images/03.png" :
+              (scorePerCent >= 20) ? "assets/images/02.png" :
+              "assets/images/01.png";
     
     scoreDiv.innerHTML = "<img src="+ img +">";
     scoreDiv.innerHTML += "<p>"+ scorePerCent +"%</p>";
