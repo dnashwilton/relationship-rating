d756cdce5d351ff4548064205aaeb642f4b2d34c
David Nash <dnashwilton@gmail.com>
2023-06-04 18:51:06 +0000

    Amended various statements in js file.

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,107 +16,91 @@ const progress = document.getElementById("progress");
 const scoreDiv = document.getElementById("scoreContainer");
 
 // create our questions
-let questions = [
-    {
-        question : "1 What does HTML stand for?",
-        // imgSrc : "img/html.png",
-        choiceA : "Correct",
-        choiceB : "Wrong",
-        choiceC : "Wrong",
-        correct : "A"
-    },{
-        question : "2 What does CSS stand for?",
-        // imgSrc : "img/css.png",
-        choiceA : "Wrong",
-        choiceB : "Correct",
-        choiceC : "Wrong",
-        correct : "B"
-    },{
-        question : "3 What does JS stand for?",
-        // imgSrc : "img/js.png",
-        choiceA : "Wrong",
-        choiceB : "Wrong",
-        choiceC : "Correct",
-        correct : "C"
-    },{
-        question : "What does HTML stand for?",
-        // imgSrc : "img/html.png",
-        choiceA : "Correct",
-        choiceB : "Wrong",
-        choiceC : "Wrong",
-        correct : "A"
-    },{
-        question : "What does CSS stand for?",
-        // imgSrc : "img/css.png",
-        choiceA : "Wrong",
-        choiceB : "Correct",
-        choiceC : "Wrong",
-        correct : "B"
-    },{
-        question : "4 What does JS stand for?",
-        // imgSrc : "img/js.png",
-        choiceA : "Wrong",
-        choiceB : "Wrong",
-        choiceC : "Correct",
-        correct : "C"
-    },{
-        question : "5 What does HTML stand for?",
-        // imgSrc : "img/html.png",
-        choiceA : "Correct",
-        choiceB : "Wrong",
-        choiceC : "Wrong",
-        correct : "A"
-    },{
-        question : "6 What does CSS stand for?",
-        // imgSrc : "img/css.png",
-        choiceA : "Wrong",
-        choiceB : "Correct",
-        choiceC : "Wrong",
-        correct : "B"
-    },{
-        question : "7 What does JS stand for?",
-        // imgSrc : "img/js.png",
-        choiceA : "Wrong",
-        choiceB : "Wrong",
-        choiceC : "Correct",
-        correct : "C"
-    },{
-        question : "8 What does HTML stand for?",
-        // imgSrc : "img/html.png",
-        choiceA : "Correct",
-        choiceB : "Wrong",
-        choiceC : "Wrong",
-        correct : "A"
-    },{
-        question : "9 What does CSS stand for?",
-        // imgSrc : "img/css.png",
-        choiceA : "Wrong",
-        choiceB : "Correct",
-        choiceC : "Wrong",
-        correct : "B"
-    },{
-        question : "10 What does JS stand for?",
-        // imgSrc : "img/js.png",
-        choiceA : "Wrong",
-        choiceB : "Wrong",
-        choiceC : "Correct",
-        correct : "C"
-    },{
-        question : "11 What does CSS stand for?",
-        // imgSrc : "img/css.png",
-        choiceA : "Wrong",
-        choiceB : "Correct",
-        choiceC : "Wrong",
-        correct : "B"
-    },{
-        question : "12 What does JS stand for?",
-        // imgSrc : "img/js.png",
-        choiceA : "Wrong",
-        choiceB : "Wrong",
-        choiceC : "Correct",
-        correct : "C"
-    }
-];
+let questions = [{
+    question: "1 What does HTML stand for?",
+    // imgSrc : "img/html.png",
+    choiceA: "Correct",
+    choiceB: "Wrong",
+    choiceC: "Wrong",
+    correct: "A"
+}, {
+    question: "2 What does CSS stand for?",
+    // imgSrc : "img/css.png",
+    choiceA: "Wrong",
+    choiceB: "Correct",
+    choiceC: "Wrong",
+    correct: "B"
+}, {
+    question: "3 What does JS stand for?",
+    // imgSrc : "img/js.png",
+    choiceA: "Wrong",
+    choiceB: "Wrong",
+    choiceC: "Correct",
+    correct: "C"
+}, {
+    question: "4 What does HTML stand for?",
+    // imgSrc : "img/html.png",
+    choiceA: "Correct",
+    choiceB: "Wrong",
+    choiceC: "Wrong",
+    correct: "A"
+}, {
+    question: "5 What does CSS stand for?",
+    // imgSrc : "img/css.png",
+    choiceA: "Wrong",
+    choiceB: "Correct",
+    choiceC: "Wrong",
+    correct: "B"
+}, {
+    question: "6 What does JS stand for?",
+    // imgSrc : "img/js.png",
+    choiceA: "Wrong",
+    choiceB: "Wrong",
+    choiceC: "Correct",
+    correct: "C"
+}, {
+    question: "7 What does HTML stand for?",
+    // imgSrc : "img/html.png",
+    choiceA: "Correct",
+    choiceB: "Wrong",
+    choiceC: "Wrong",
+    correct: "A"
+}, {
+    question: "8 What does CSS stand for?",
+    // imgSrc : "img/css.png",
+    choiceA: "Wrong",
+    choiceB: "Correct",
+    choiceC: "Wrong",
+    correct: "B"
+}, {
+    question: "9 What does JS stand for?",
+    // imgSrc : "img/js.png",
+    choiceA: "Wrong",
+    choiceB: "Wrong",
+    choiceC: "Correct",
+    correct: "C"
+}, {
+    question: "10 What does HTML stand for?",
+    // imgSrc : "img/html.png",
+    choiceA: "Correct",
+    choiceB: "Wrong",
+    choiceC: "Wrong",
+    correct: "A"
+}, {
+    question: "11 What does CSS stand for?",
+    // imgSrc : "img/css.png",
+    choiceA: "Wrong",
+    choiceB: "Correct",
+    choiceC: "Wrong",
+    correct: "B"
+}, {
+    question: "12 What does JS stand for?",
+    // imgSrc : "img/js.png",
+    choiceA: "Wrong",
+    choiceB: "Wrong",
+    choiceC: "Correct",
+    correct: "C"
+}];
 
 // create some variables
 
@@ -130,54 +114,54 @@ let TIMER;
 let score = 0;
 
 // render a question
-function renderQuestion(){
+function renderQuestion() {
     let q = questions[runningQuestion];
-    
-    question.innerHTML = "<p>"+ q.question +"</p>";
+
+    question.innerHTML = "<p>" + q.question + "</p>";
     // qImg.innerHTML = "<img src="+ q.imgSrc +">";
     choiceA.innerHTML = q.choiceA;
     choiceB.innerHTML = q.choiceB;
     choiceC.innerHTML = q.choiceC;
 }
 
-start.addEventListener("click",startQuiz);
+start.addEventListener("click", startQuiz);
 
 // start quiz
-function startQuiz(){
+function startQuiz() {
     start.style.display = "none";
     renderQuestion();
     quiz.style.display = "block";
     renderProgress();
     renderCounter();
-    // TIMER = setInterval(renderCounter,1000); // 1000ms = 1s
+    TIMER = setInterval(renderCounter, 1000); // 1000ms = 1s
 }
 
-alpha.addEventListener("click",renderProgress);
+/*alpha.addEventListener("click",renderProgress);
 bravo.addEventListener("click",renderProgress);
-charlie.addEventListener("click",renderProgress);
+charlie.addEventListener("click",renderProgress);*/
 
 // render progress
-function renderProgress(){
-    for(let qIndex = 0; qIndex <= lastQuestion; qIndex++){
-        progress.innerHTML += "<div class='prog' id="+ qIndex +"></div>";
+function renderProgress() {
+    for (let qIndex = 0; qIndex <= lastQuestion; qIndex++) {
+        progress.innerHTML += "<div class='prog' id=" + qIndex + "></div>";
     }
 }
 
 // counter render
 
-function renderCounter(){
-    if(count <= questionTime){
+function renderCounter() {
+    if (count <= questionTime) {
         counter.innerHTML = count;
         timeGauge.style.width = count * gaugeUnit + "px";
         count++
-    }else{
+    } else {
         count = 0;
         // change progress color to red
         answerIsWrong();
-        if(runningQuestion < lastQuestion){
+        if (runningQuestion < lastQuestion) {
             runningQuestion++;
             renderQuestion();
-        }else{
+        } else {
             // end the quiz and show the score
             clearInterval(TIMER);
             scoreRender();
@@ -187,22 +171,22 @@ function renderCounter(){
 
 // checkAnwer
 
-function checkAnswer(answer){
-    if( answer == questions[runningQuestion].correct){
+function checkAnswer(answer) {
+    if (answer == questions[runningQuestion].correct) {
         // answer is correct
         score++;
         // change progress color to green
         answerIsCorrect();
-    }else{
+    } else {
         // answer is wrong
         // change progress color to red
         answerIsWrong();
     }
     count = 0;
-    if(runningQuestion < lastQuestion){
+    if (runningQuestion < lastQuestion) {
         runningQuestion++;
         renderQuestion();
-    }else{
+    } else {
         // end the quiz and show the score
         clearInterval(TIMER);
         scoreRender();
@@ -210,61 +194,61 @@ function checkAnswer(answer){
 }
 
 // answer is correct
-function answerIsCorrect(){
+function answerIsCorrect() {
     document.getElementById(runningQuestion).style.backgroundColor = "#0f0";
 }
 
 // answer is Wrong
-function answerIsWrong(){
+function answerIsWrong() {
     document.getElementById(runningQuestion).style.backgroundColor = "#f00";
 }
 
 // score render
-function scoreRender(){
+function scoreRender() {
     scoreDiv.style.display = "block";
-    
+
     // calculate the amount of question percent answered by the user
-    const scorePerCent = Math.round(100 * score/questions.length);
+    const scorePerCent = Math.round(100 * score / questions.length);
 
     // Match case to answers
 
-   /* Select Case CorrectAnswers
+    /*switch (score) {
 
-Case 0 To 3
-Message = "This score shows that your knowledge about sex is very basic. "
-Message = Message + "Finding out more about it can only be to your advantage."
+        case "1 To 3":
+            endOfQuizMessage = `This score shows that your knowledge about sex is very basic. 
+Finding out more about it can only be to your advantage.`;
+            break;
 
-MsgBox Message, DialogType, Title
+        case "4 To 6":
+            endOfQuizMessage = `Your knowledge of sex is not as good as it could be. Finding out as much 
+    as you can about sex can only help to improve your relationship with 
+    your partner.`;
+            break;
 
-Case 4 To 6
-Message = "Your knowledge of sex is not as good as it could be. Finding out as much "
-Message = Message + "as you can about sex can only help to improve your relationship with "
-Message = Message + "your partner."
+        case "7 To 9":
+            endOfQuizMessage = `This is a fair score but you might like to learn more. Try to broaden your knowledge 
+    by reading more about the subject.`;
+            break;
 
-MsgBox Message, DialogType, Title
+        case "10 To 12";
+        endOfQuizMessage = `A very impressive performance. If you are as warm and considerate a lover as you are knnowledgeable "
+    about sex, your partner should be a very happy person.`;
+        break;
 
-Case 8 To 10
-Message = "THis is a fair score but you might like to learn more. Try to broaden your knowledge "
-Message = Message + "by reading more about the subject."
+        default:
+            window.alert("No score!");
+            break;
 
-MsgBox Message, DialogType, Title
+            End Select
+    }*/
 
-Case 11 To 12
-Message = "A very impressive performance. If you are as warm and considerate a lover as you are knnowledgeable "
-Message = Message = "about sex, your partner should be a very happy person."
-
-MsgBox Message, DialogType, Title
-
-End Select
-*/
-    
     // choose the image based on the scorePerCent
     let img = (scorePerCent >= 80) ? "assets/images/05.png" :
-              (scorePerCent >= 60) ? "assets/images/04.png" :
-              (scorePerCent >= 40) ? "assets/images/03.png" :
-              (scorePerCent >= 20) ? "assets/images/02.png" :
-              "assets/images/01.png";
-    
-    scoreDiv.innerHTML = "<img src="+ img +">";
-    scoreDiv.innerHTML += "<p>"+ scorePerCent +"%</p>";
+        (scorePerCent >= 60) ? "assets/images/04.png" :
+        (scorePerCent >= 40) ? "assets/images/03.png" :
+        (scorePerCent >= 20) ? "assets/images/02.png" :
+        "assets/images/01.png";
+
+    scoreDiv.innerHTML = "<img src=" + img + ">";
+    scoreDiv.innerHTML += "<p>" + scorePerCent + "%</p>";
 }
